4415c48369e631dfa46674915d5e7a9197eac189
Rob Eastmond <robeastmond@gmail.com>
2021-09-07 13:58:03 -0600

    added attack

--- monster_mash.py
+++ monster_mash.py
@@ -1,34 +1,41 @@
 from creature import *
 import re
+import random
 
 print("Welcome to Monster Mash!")
 # Game Settings
 battlefield = []
 numPlayers = ''
-charClass = ['Elf','Fairy','Orc','Duck']
+charClass = ['Elf','Fairy','Orc']
 
 # Validation Loop for number of players
 while re.search(r'^\d+$',numPlayers) == None:
   numPlayers = input('How many players are playing? (integers) ')
 numPlayers = int(numPlayers)
 
-
 # Set up the players
 # Loop to set up the characters
 for i in range(numPlayers):
   # What character type?
   choice = ''
-  while choice not in enumerate(charClass):
+  while choice not in list(enumerate(charClass)):
     choice = ''
     for i,cClass in enumerate(charClass):
       print(f'{i}: {cClass}')
     while not choice.isnumeric():
       choice = input('Choose a character from the list above: ')
     choice = int(choice)
+    break 
   # Ask for the name?
+  name = input(f'What is your {charClass[choice]}\'s name? ')
+  # Random stats for simplicity ToDo
+  # Create the character
+  temp_char = eval(charClass[choice])(name)
+  battlefield.append(temp_char)
 
-  # Random stats for simplicity
+battlefield[0].attack(battlefield[1])
 
+  
 
 """                                    
 # Max W
