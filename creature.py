4afd8586da68ead7e85009b0966d4ad544927a4b
Rob Eastmond <RobbyJJr@users.noreply.github.com>
2021-09-07 14:07:39 -0600

    Add files via upload

--- creature.py
+++ creature.py
@@ -1,10 +1,12 @@
 class Orc:
-  def __init__ (self, _health, _damage, _speed):
+  def __init__ (self, _name,_health=100, _damage=50, _speed=10):
+    self.name = _name
     self.health = _health
     self.damage = _damage
     self.speed = _speed
-  def attack(self):
-    print('Attack')
+  def attack(self, _target):
+    #print(self.name + ' Attacked ' + _target.name)
+    print(f'{self.name} Attacked {_target.name}')
   def defend(self):
     print('Defend')
   def heal(self):
@@ -16,13 +18,15 @@ class Orc:
 
 
 class Fairy:
-  def __init__ (self, _health, _damage, _magic_power, _speed):
+  def __init__ (self,_name, _health = 50, _damage = 25, _magic_power = 150, _speed = 50):
+    self.name = _name
     self.health = _health
     self.damage = _damage
     self.magic_power = _magic_power
     self.speed = _speed
-  def attack(self):
-    print('Attack')
+  def attack(self, _target):
+    #print(self.name + ' Attacked ' + _target.name)
+    print(f'{self.name} Attacked {_target.name}')
   def defend(self):
     print('Defend')
   def heal(self):
@@ -36,7 +40,8 @@ class Fairy:
 
 
 class Elf:
-  def __init__ (self, _health, _damage, _magic_power, _speed):
+  def __init__ (self,_name, _health = 75, _damage = 50, _magic_power = 100, _speed = 100):
+    self.name = _name
     self.health = _health
     self.damage = _damage
     self.magic_power = _magic_power
